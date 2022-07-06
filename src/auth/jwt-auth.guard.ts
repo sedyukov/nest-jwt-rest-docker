f44c32b5b0591ee7929c54a25c16577f0cc422a3
sedyukov <sedyukovyaroslav@gmail.com>
2022-07-06 07:44:36 +0700

    feat: auth guard

---
+++ src/auth/jwt-auth.guard.ts
@@ -0,0 +1,24 @@
+import {CanActivate, ExecutionContext, UnauthorizedException} from "@nestjs/common";
+import {Observable} from "rxjs";
+import {JwtService} from "@nestjs/jwt";
+
+export class JwtAuthGuard implements CanActivate {
+
+    constructor(private jwtService: JwtService) {}
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const req = context.switchToHttp().getRequest()
+        try {
+            const authHeader = req.headers.authorization;
+            const [bearer, token] = authHeader.split(' ')
+            if (bearer !== 'Bearer' || !token) {
+                throw new UnauthorizedException({message: 'Unauthorized user'})
+            }
+            const user = this.jwtService.verify(token)
+            req.user = user
+            return true
+        } catch (e) {
+            throw new UnauthorizedException({message: 'Unauthorized user'})
+        }
+    }
+}
